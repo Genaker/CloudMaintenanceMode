2fc21c55cd0a807940ea4fed6fec1896891ecdf0
Egor Shitikov <egorshitikov@gmail.com>
2021-06-27 17:01:00 -0700

    Update MaintenanceMode.php

--- Genaker/CloudMaintenanceMode/MaintenanceMode.php
+++ Genaker/CloudMaintenanceMode/MaintenanceMode.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace Genaker\CloudMaintenanceMode\Rewrite;
+namespace Genaker\CloudMaintenanceMode;
 
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem;
